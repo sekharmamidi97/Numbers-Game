6a59963acddcadad131dfa57a6e610ded4933759
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-01-23 14:55:15 -0500

    First commit

---
+++ NumbersGame/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Numbers Game!");
+            bool likeToPlay = true;
+            
+            while (likeToPlay)
+            {
+                Console.WriteLine("Please enter any number: ");
+                string userChoice = Console.ReadLine();
+                int userPick = int.Parse(userChoice);
+
+                Random random = new Random();
+                int computerPick = random.Next(1,1000);
+
+                if (computerPick > userPick)
+                {
+                    Console.WriteLine("Rats! The computer picked a larger number and you lose!");
+                }
+
+                else if (computerPick < userPick)
+                {
+                    Console.WriteLine("Awesome! You picked a larger number than the computer and you win!");
+                }
+
+                else if (computerPick == userPick)
+                {
+                    Console.WriteLine("You picked the same number as the computer and it's a tie!");
+                }
+
+                else
+                {
+                    Console.WriteLine("You must enter a number!");
+                }
+
+                Console.WriteLine("Do you want play this game again? y = yes or n = no");
+                ConsoleKeyInfo key = Console.ReadKey();
+                Console.WriteLine();
+                likeToPlay = key.KeyChar == 'y';
+
+            }
+
+        }
+    }
+}
+
