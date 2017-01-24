87e76b2dc35b786e72e07a7175cea37d300dddfe
LAPTOP-4E83PLLR\Sekhar Mamidi <sekhar.mamidi.97@gmail.com>
2017-01-24 12:38:48 -0500

    updated project

--- NumbersGame/Program.cs
+++ NumbersGame/Program.cs
@@ -15,38 +15,39 @@ namespace NumbersGame
             
             while (likeToPlay)
             {
-                Console.WriteLine("Please enter any number: ");
-                string userChoice = Console.ReadLine();
-                int userPick = int.Parse(userChoice);
-
-                Random random = new Random();
-                int computerPick = random.Next(1,1000);
-
-                if (computerPick > userPick)
+                try
                 {
-                    Console.WriteLine("Rats! The computer picked a larger number and you lose!");
-                }
+                    Console.WriteLine("Please enter any number: ");
+                    string userChoice = Console.ReadLine();
+                    int userPick = int.Parse(userChoice);
 
-                else if (computerPick < userPick)
-                {
-                    Console.WriteLine("Awesome! You picked a larger number than the computer and you win!");
-                }
+                    Random random = new Random();
+                    int computerPick = random.Next(1,5);
 
-                else if (computerPick == userPick)
-                {
-                    Console.WriteLine("You picked the same number as the computer and it's a tie!");
-                }
+                    if (computerPick > userPick)
+                    {
+                        Console.WriteLine("Rats! The computer picked a larger number and you lose!");
+                    }
 
-                else
+                    else if (computerPick < userPick)
+                    {
+                        Console.WriteLine("Awesome! You picked a larger number than the computer and you win!");
+                    }
+
+                    else 
+                    {
+                        Console.WriteLine("You picked the same number as the computer and it's a tie!");
+                    }
+
+                    Console.WriteLine("Do you want play this game again? y = yes or n = no");
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    Console.WriteLine();
+                    likeToPlay = key.KeyChar == 'y';
+                }
+                catch(FormatException ex)
                 {
                     Console.WriteLine("You must enter a number!");
                 }
-
-                Console.WriteLine("Do you want play this game again? y = yes or n = no");
-                ConsoleKeyInfo key = Console.ReadKey();
-                Console.WriteLine();
-                likeToPlay = key.KeyChar == 'y';
-
             }
 
         }
